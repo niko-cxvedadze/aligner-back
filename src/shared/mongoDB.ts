982c0119c299c96ecb5678d1ff82bcac413e3943
niko-cxvedadze <nikolozcxvedadze@gmail.com>
2024-07-06 02:29:17 +0400

    database

--- src/shared/mongoDB.ts
+++ src/shared/mongoDB.ts
@@ -2,11 +2,9 @@ import mongoose from 'mongoose';
 
 export async function connectMongoDB() {
   try {
-    await mongoose.connect(`mongodb://localhost:27017/`, {
-      user: 'root',
-      pass: 'root',
-      dbName: 'aligner-back',
-    });
+    await mongoose.connect(
+      `mongodb+srv://admin:admin@aligner-test.giiq7pf.mongodb.net/?retryWrites=true&w=majority&appName=aligner-test`,
+    );
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB: ', error);
